c0f8ec92d019c1dc632c468e8619e8314692bfe6
Victor Garcia <victor.garciagutierrez.itcm@gmail.com>
2024-06-16 12:35:54 -0600

    Se incluye un loading al cargar los paises y se agrega el OnDestroy para que limpie la memoria.

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, input } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
+import { serialize } from 'v8';
 
 @Component({
   selector: 'shared-search-box',
@@ -9,7 +11,13 @@ import { Component, EventEmitter, Input, Output, input } from '@angular/core';
     }
   `,
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+
+  //Es un tipo especial de observable
+  private debouncer: Subject<string> = new Subject<string>();
+
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string  = '';
@@ -17,8 +25,29 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncerSuscription = this.debouncer
+    .pipe(
+      debounceTime(1000)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit( value );
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   emitValue( value:string): void {
     this.onValue.emit( value );
+  }
+
+  onKeyPress( searchTerm: string) {
+    this.debouncer.next( searchTerm );
   }
 
  }
